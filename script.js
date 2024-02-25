2116f85ab727dc35396359cada89a54cff88dcea
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-25 14:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function createTable() {
-    //Write your code here
-  
+	let row = prompt("Enter row: ") 	
+	let col = prompt("Enter col: ")
+
+	let table = document.getElementById("myTable");
+	table.innerHTML = "";
+	
+
+	for (let i = 0; i < row; i++) {
+		let row = table.insertRow(i);
+		for (let j = 0; j < col; j++) {
+			let cell = row.insertCell(j);	
+			cell.textContent = "Row-" + i + " Column-" + j;
+		}
+	}
 }
